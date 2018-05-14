51467960b7aefa7a025483aef74267372b5f90f5
Zachariah Bendas <zbendas@yahoo.com>
2018-05-14 16:28:50 -0500

    Committing dirty files for later work

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div class="app-body">
+        <form @submit.prevent>
+            <input class="search-box" type="text" title="Search" placeholder="e.g.: dagger, gauntlet, longspear"
+                   v-model="search_text"/>
+        </form>
+        <equipment-result v-for="item in items" :key="item.name"
+                          v-bind:item="item"
+                          v-bind:filter="search_text"
+        />
+    </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+    import EquipmentResult from "./components/EquipmentResult.vue"
 
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
+    export default {
+        name: "app",
+        components: {
+            EquipmentResult,
+        },
+        data: function () {
+            return {
+                items: [
+                    {
+                        "item_type": "Unarmed",
+                        "name": "Gauntlet",
+                        "cost": "2gp",
+                        "damage_small": "1d2",
+                        "damage_medium": "1d3",
+                        "critical_multiplier": "2",
+                        "weight": "1 lb.",
+                        "damage_type": ["B"],
+                    },
+                    {
+                        "item_type": "Light",
+                        "name": "Dagger",
+                        "cost": "2gp",
+                        "damage_small": "1d3",
+                        "damage_medium": "1d4",
+                        "critical_range": "19-20",
+                        "critical_multiplier": "2",
+                        "range": "10 ft.",
+                        "weight": "1 lb.",
+                        "damage_type": ["P or S"]
+                    },
+                    {
+                        "item_type": "Two-handed",
+                        "name": "Longspear",
+                        "cost": "5gp",
+                        "damage_small": "1d6",
+                        "damage_medium": "1d8",
+                        "critical_multiplier": "3",
+                        "weight": "9 lbs.",
+                        "damage_type": ["P"],
+                        "special": ["brace", "reach"]
+                    }
+                ],
+                search_text: ""
+            }
+        }
+    };
 </script>
 
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+    @import url('https://fonts.googleapis.com/css?family=Montserrat:500')
+    #app
+        background-color: white
+        width: 100vw
+        height: 100vw
+
+    .app-body
+        margin: 5vh 20vw
+
+    .search-box
+        width: 100%
+        height: 2em
+        background: #333333
+        border: none
+        color: #eeeeee
+        padding: 4px
 </style>
---
+++ src/components/EquipmentResult.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="equipment_result"
+         v-on:mouseover="onHover"
+         v-on:click="showItem"
+         v-bind:class="{expanded: expanded}"
+         v-show=" item.name.toLowerCase() === filter.toLowerCase() || filter === ''"
+    >
+        <div>{{ item.name }}</div>
+        <div class="equipment_info" v-show="expanded" v-bind:class="[item.item_type.toLowerCase()]">
+            <div>
+                Type: {{ item.item_type }}
+                Cost: {{ item.cost ? item.cost : "—" }}
+            </div>
+            Damage (S)/(M): {{ item.damage_small ? item.damage_small : "—" }} / {{ item.damage_medium ?
+            item.damage_medium : "—" }}
+            Critical: {{ item.critical_range ? item.critical_range + "/" : "" }}{{ "×" + item.critical_multiplier }}
+            Range: {{ item.range ? item.range : "—" }}
+            Weight: {{ item.weight ? item.weight : "—" }}
+            Damage Type: {{ item.damage_type }}
+            Special: {{ item.special ? item.special : "—" }}
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "EquipmentResult",
+        props: {
+            item: Object,
+            filter: String,
+        },
+        data: function () {
+            return {
+                expanded: false
+            }
+        },
+        methods: {
+            onHover: function (event) {
+                //console.log(this.item_name + " was hovered!");
+            },
+            showItem: function (event) {
+                //console.log("This item's name is " + this.item_name);
+                this.expanded ? this.expanded = false : this.expanded = true;
+            }
+        }
+    }
+</script>
+
+<style scoped lang="sass">
+    .equipment_result
+        font-family: Montserrat, sans-serif
+        font-weight: 500
+        font-size: 1.5em
+        .equipment_info
+            font-family: sans-serif
+            font-weight: normal
+            font-size: 0.75em
+        .unarmed
+            background-color: palegoldenrod
+
+        .light
+            background-color: palegreen
+
+        .two-handed
+            background-color: palevioletred
+</style>
